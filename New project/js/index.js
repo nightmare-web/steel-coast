465f8f6d2c2ef0f85f99561ce55c4d861c2ee3eb
nightmare-web <Taylor.133@yandex.ru>
2026-05-19 21:55:20 +0500

    Delete New project directory

--- New project/js/index.js
+++
@@ -1,7 +0,0 @@
-import { initSiteHeader } from "./site-header.js";
-import { initFaq } from "./faq.js";
-import { initScrollTop } from "./scroll-top.js";
-
-initSiteHeader();
-initFaq();
-initScrollTop();
